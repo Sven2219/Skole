abd264e4d7a061f19d3c85bd50c27c07dbfef8e5
Sven2219 <sven.suk5@gmail.com>
2021-01-11 12:29:22 +0100

    wrong code pushed

--- index.js
+++ index.js
@@ -3,9 +3,8 @@
  */
 
 import { AppRegistry } from 'react-native';
-import App from './src/navigation/Navigation';
+
 import { name as appName } from './app.json';
-import AllCountries from './src/fragments/AllCountries';
-import COVIDInfo from './src/fragments/COVIDInfo';
-import SearchCountries from './src/fragments/SearchCountries';
-AppRegistry.registerComponent(appName, () => SearchCountries);
+import Navigation from './src/navigation/Navigation';
+
+AppRegistry.registerComponent(appName, () => Navigation);
